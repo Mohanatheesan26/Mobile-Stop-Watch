039d88553c1e8e58b5cd412039685200870c9f5f
Mohan <mohanatheesan@gmail.com>
2023-01-07 14:14:43 +0530

    Update StopWatch

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { DetailComponent } from './single-post/single-post.component';
+import { HomeComponent } from './home/home.component';
+import { StopWatchComponent } from './stop-watch/stop-watch.component';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'stop-watch',
+    component: StopWatchComponent,
+  },
+  {
+    path: ':id',
+    component: DetailComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
